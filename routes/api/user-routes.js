2f33f596ab0fc6bba7b799794cbf4ee9b6ffb814
Christi-Lee <christileemc@gmail.com>
2021-06-17 21:33:22 -0700

    added code snapshot per the module at 13.1.7

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,41 +1,38 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-// GET /api/users
+// get all users
 router.get('/', (req, res) => {
-    // Access our User model and run .findAll() method)
-    User.findAll()
+  User.findAll({
     attributes: { exclude: ['password'] }
-})
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  
+  })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-// GET /api/users/1
 router.get('/:id', (req, res) => {
-      User.findOne({
-        attributes: { exclude: ['password'] },
-        where: {
-          id: req.params.id
-        }
-      })
-        .then(dbUserData => {
-          if (!dbUserData) {
-            res.status(404).json({ message: 'No user found with this id' });
-            return;
-          }
-          res.json(dbUserData);
-        })
-        .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-        });
-  });
+  User.findOne({
+    attributes: { exclude: ['password'] },
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-// POST /api/users
 router.post('/', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
@@ -50,11 +47,10 @@ router.post('/', (req, res) => {
     });
 });
 
-// PUT /api/users/1
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
-  // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+  // pass in req.body instead to only update what's passed through
   User.update(req.body, {
     where: {
       id: req.params.id
@@ -72,7 +68,7 @@ router.put('/:id', (req, res) => {
       res.status(500).json(err);
     });
 });
-// DELETE /api/users/1
+
 router.delete('/:id', (req, res) => {
   User.destroy({
     where: {
@@ -91,8 +87,5 @@ router.delete('/:id', (req, res) => {
       res.status(500).json(err);
     });
 });
-
-// DELETE /api/users/1
-router.delete('/:id', (req, res) => {});
 
 module.exports = router;
