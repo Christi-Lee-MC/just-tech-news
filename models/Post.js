3e41810ed4d49092574ade5dd6957c06ca006517
Christi-Lee <christileemc@gmail.com>
2021-06-21 22:12:33 -0700

    created post models and routes

--- models/Post.js
+++ models/Post.js
@@ -40,4 +40,24 @@ Post.init(
     }
   );
 
+  // get all users
+router.get('/', (req, res) => {
+    Post.findAll({
+      attributes: ['id', 'post_url', 'title', 'created_at'],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
   module.exports = Post;
+
+  module.exports = router;
