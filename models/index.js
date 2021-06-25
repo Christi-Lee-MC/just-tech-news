4758b601b162ce97e09bd56b7d6f917d5af923d5
Christi-Lee <christileemc@gmail.com>
2021-06-25 13:43:35 -0700

    added code per module 13.3 end

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 // import all models
-const User = require("./User");
-const Post = require("./Post");
+const Post = require('./Post');
+const User = require('./User');
 
 // create associations
 User.hasMany(Post, {
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User, Vote} = require('../../models');
+const { Post, User } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -20,7 +20,6 @@ router.get('/', (req, res) => {
     });
 });
 
-// find one user
 router.get('/:id', (req, res) => {
   Post.findOne({
     where: {
@@ -47,7 +46,6 @@ router.get('/:id', (req, res) => {
     });
 });
 
-// post create 
 router.post('/', (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
@@ -60,43 +58,6 @@ router.post('/', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
-});
-
-// create the vote
-Vote.create({
-    user_id: req.body.user_id,
-    post_id: req.body.post_id
-  }).then(() => {
-    // then find the post we just voted on
-    return Post.findOne({
-      where: {
-        id: req.body.post_id
-      },
-      attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-        // use raw MySQL aggregate function query to get a count of how many votes the post has and return it under the name `vote_count`
-        [
-          sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-          'vote_count'
-        ]
-      ]
-    })
-    .then(dbPostData => res.json(dbPostData))
-    .catch(err => {
-      console.log(err);
-      res.status(400).json(err);
-    });
-// PUT /api/posts/upvote
-router.put('/upvote', (req, res) => {
-    Vote.create({
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-      })
-        .then(dbPostData => res.json(dbPostData))
-        .catch(err => res.json(err));
 });
 
 router.put('/:id', (req, res) => {
